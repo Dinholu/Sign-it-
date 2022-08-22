de4cf9b6a17b303e71dcf020ff4a292b1122dc19
Alizee <alizee.hett@t-med.fr>
2022-08-22 14:15:14 +0200

    test setasign

---
+++ resources/js/Pages/hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        bonjour
+        {{this.hello}}
+        <input type="file" name="" id="" accept="application/pdf,application/vnd.ms-excel">
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Hello',
+        props: {
+            hello: {
+                type: String,
+            }
+        },
+        mounted() {
+            console.log(this.hello);
+        }
+    }
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
+use Illuminate\Support\Str;
+use Dotenv\Store\File\Paths;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Storage;
+use setasign\Fpdi\Fpdi;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,4 +31,46 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+# test pour pdf
+Route::get('/hello', function () {
+    $hello = Str::random($length = 15);
+
+
+    // $outputFile = "./public/hello.pdf";
+    // header('Content-type: application/pdf');
+    // header('Content-Disposition: inline; filename="' . $outputFile . '"');
+    // header('Content-Transfer-Encoding: binary');
+    // header('Accept-Ranges: bytes');
+    // @readfile($outputFile);
+
+
+    $pdf = new Fpdi();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', 'B', 16);
+    $path = public_path('/hello.pdf');
+    $pdf->setSourceFile($path);
+    $tplId = $pdf->importPage(1);
+    // use the imported page and place it at point 10,10 with a width of 100 mm
+    $pdf->useTemplate($tplId, null, null, null, 210, true);
+
+    // Now this details we are going to print in pdf.
+    // Horizontal and veritcal setXY
+
+
+    $pdf->SetXY(40, 40);
+    // Details you want to print
+
+    // Now let's change details an position
+    $pdf->Write(0.1, "BBUniversal");
+
+    // let's bring another below it
+
+    // Second details
+    $pdf->SetXY(40, 50);
+    $pdf->Write(0.1, "Thank you. If you have any doubt.");
+
+    $pdf->SetXY(40, 60);
+    $pdf->Write(0.1, "Feel free to comment.");
+    ddd($pdf);
+});
+require __DIR__ . '/auth.php';
